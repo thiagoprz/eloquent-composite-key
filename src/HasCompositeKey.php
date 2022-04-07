46d20323b4950401768141e646bd04e113d2f6c4
Thiago Przyczynski <przyczynski@gmail.com>
2022-04-07 05:51:37 -0300

    Enhancement for querying modelby primary keys

--- src/HasCompositeKey.php
+++ src/HasCompositeKey.php
@@ -1,5 +1,8 @@
 <?php
+
 namespace Thiagoprz\CompositeKey;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 /**
  * Trait HasCompositeKey
@@ -22,5 +25,88 @@ trait HasCompositeKey
                 $q->where($key, '=', $this->getAttribute($key));
             }
         });
+    }
+
+    /**
+     * Get the casts array.
+     *
+     * @return array
+     */
+    public function getCasts()
+    {
+        if ($this->getIncrementing()) {
+            return array_merge([$this->getKeyName() => $this->getKeyType()], $this->casts);
+        }
+        return $this->casts;
+    }
+
+    /**
+     * @return false
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    /**
+     * Get the value of the model's primary key.
+     *
+     * @return mixed
+     */
+    public function getKey()
+    {
+        $fields = $this->getKeyName();
+        $keys = [];
+        array_map(function($key) use($fields, &$keys) {
+            $keys[] = $this->getAttribute($key);
+        }, $fields);
+        return $keys;
+    }
+
+    /**
+     * Finds model by primary keys
+     *
+     * @param array $ids
+     * @return mixed
+     */
+    public function find(array $ids)
+    {
+        if (!isset($this)) {
+            $modelClass = self::class;
+            $model = new $modelClass();
+        } else {
+            $model = $this;
+        }
+        $keys = $model->primaryKey;
+        return $model->where(function($query) use($ids, $keys) {
+            foreach ($keys as $idx => $key) {
+                if (isset($ids[$idx])) {
+                    $query->where($key, $ids[$idx]);
+                } else {
+                    $query->whereNull($key);
+                }
+            }
+        })->first();
+    }
+
+    /**
+     * Find model by primary key or throws ModelNotFoundException
+     *
+     * @param array $ids
+     * @return mixed
+     */
+    public function findOrFail(array $ids)
+    {
+        if (!isset($this)) {
+            $modelClass = self::class;
+            $model = new $modelClass();
+        } else {
+            $model = $this;
+        }
+        $record = $model->find($ids);
+        if (!$record) {
+            throw new ModelNotFoundException;
+        }
+        return $record;
     }
 }
